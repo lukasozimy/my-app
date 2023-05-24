6ba15a4b082ccb6d136c21a1530bc861d313c421
lukasozimy <lukas.ozimy@oznet.sk>
2023-05-24 17:09:44 +0200

    modified:   package.json 	modified:   src/App.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ root.render(
   </React.StrictMode>
 );
 
+const port = process.env.PORT || 3000;
+App.listen(port,() => console.log(`Server running on port ${port}`));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
